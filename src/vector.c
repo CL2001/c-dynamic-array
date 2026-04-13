41b89fcabcf2d0fe426338bbe88dd22faee44a5e
Christophe <christophe.lanouette@gmail.com>
2026-04-13 19:08:19 -0400

    Add push back and get

--- src/vector.c
+++ src/vector.c
@@ -25,3 +25,32 @@ void vector_free(Vector* v)
     v->size = 0;
     v->capacity = 0;
 }
+
+
+void vector_push_back(Vector* v, void* elem)
+{
+    if (v->size >= v->capacity)
+    {
+        v->capacity *= 2;
+        v->data = realloc(v->data, v->elem_size * v->capacity);
+        if (!v->data)
+        {
+            fprintf(stderr, "vector_push_back: realloc failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    memcpy((char*)v->data + v->size * v->elem_size, elem, v->elem_size);
+    v->size++;
+}
+
+
+void* vector_get(Vector* v, int i)
+{
+    if (i < 0 || i >= v->size)
+    {
+        fprintf(stderr, "vector_get: index %d out of bounds (size=%d)\n", i, v->size);
+        exit(EXIT_FAILURE);
+    }
+
+    return (char*)v->data + i * v->elem_size;
+}
